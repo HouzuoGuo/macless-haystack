d624f7e76f8f6e8bfdba945fcf17473852c8fe5d
HouzuoGuo <guohouzuo@gmail.com>
2026-01-17 10:56:18 +0000

    use request_code2 method to kick off sms 2fa

--- endpoint/register/pypush_gsa_icloud.py
+++ endpoint/register/pypush_gsa_icloud.py
@@ -35,12 +35,12 @@ urllib3.disable_warnings()
 logger = logging.getLogger()
 
 
-def icloud_login_mobileme(username='', password=''):
+def icloud_login_mobileme(username="", password=""):
     print("")  # Sometimes no output
     if not username:
-        username = input('Apple ID: ')
+        username = input("Apple ID: ")
     if not password:
-        password = getpass('Password: ')
+        password = getpass("Password: ")
 
     g = gsa_authenticate(username, password)
     pet = g["t"]["com.apple.gs.idms.pet"]["token"]
@@ -56,17 +56,17 @@ def icloud_login_mobileme(username='', password=''):
     headers = {
         "X-Apple-ADSID": adsid,
         "User-Agent": "com.apple.iCloudHelper/282 CFNetwork/1408.0.4 Darwin/22.5.0",
-        "X-Mme-Client-Info": '<MacBookPro18,3> <Mac OS X;13.4.1;22F8> <com.apple.AOSKit/282 (com.apple.accountsd/113)>'
+        "X-Mme-Client-Info": "<MacBookPro18,3> <Mac OS X;13.4.1;22F8> <com.apple.AOSKit/282 (com.apple.accountsd/113)>",
     }
     headers.update(generate_anisette_headers())
 
     logger.info("Registering device after login")
     with requests.post(
-            "https://setup.icloud.com/setup/iosbuddy/loginDelegates",
-            auth=(username, pet),
-            data=data,
-            headers=headers,
-            verify=False,
+        "https://setup.icloud.com/setup/iosbuddy/loginDelegates",
+        auth=(username, pet),
+        data=data,
+        headers=headers,
+        verify=False,
     ) as resp:
         resp.raise_for_status()
     response = f"HTTP-Code: {resp.status_code}\n{resp.text}"
@@ -80,10 +80,13 @@ def gsa_authenticate(username, password):
     _, a = usr.start_authentication()
     logger.info("Authentication request initialization")
     r = gsa_authenticated_request(
-        {"A2k": a, "ps": ["s2k", "s2k_fo"], "u": username, "o": "init"})
+        {"A2k": a, "ps": ["s2k", "s2k_fo"], "u": username, "o": "init"}
+    )
 
     if r["sp"] not in ["s2k", "s2k_fo"]:
-        logger.warning(f"This implementation only supports s2k and sk2_fo. Server returned {r['sp']}")
+        logger.warning(
+            f"This implementation only supports s2k and sk2_fo. Server returned {r['sp']}"
+        )
         return
 
     # Change the password out from under the SRP library, as we couldn't calculate it without the salt.
@@ -97,7 +100,8 @@ def gsa_authenticate(username, password):
         return
     logger.info("Authentication request completion")
     resp = gsa_authenticated_request(
-        {"c": r["c"], "M1": m, "u": username, "o": "complete"})
+        {"c": r["c"], "M1": m, "u": username, "o": "complete"}
+    )
 
     # Make sure that the server's session key matches our session key (and thus that they are not an imposter)
     if "M2" not in resp:
@@ -117,12 +121,17 @@ def gsa_authenticate(username, password):
 """
     spd = plist.loads(PLISTHEADER + spd)
 
-    if "au" in resp["Status"] and resp["Status"]["au"] in ["trustedDeviceSecondaryAuth", "secondaryAuth"]:
+    logger.info(f"Login authentication response: {resp}")
+    if "au" in resp["Status"] and resp["Status"]["au"] in [
+        "trustedDeviceSecondaryAuth",
+        "secondaryAuth",
+    ]:
         logger.info("2FA required, requesting SMS code. (No other 2FA-code will work!)")
         # Replace bytes with strings
         for k, v in spd.items():
             if isinstance(v, bytes):
                 spd[k] = base64.b64encode(v).decode()
+                logger.info(f"Login authentication SPD item: key {k}, value {spd[k]}")
         sms_second_factor(spd["adsid"], spd["GsIdmsToken"])
 
         return gsa_authenticate(username, password)
@@ -144,20 +153,19 @@ def gsa_authenticated_request(parameters):
         "Content-Type": "text/x-xml-plist",
         "Accept": "*/*",
         "User-Agent": "akd/1.0 CFNetwork/978.0.7 Darwin/18.7.0",
-        "X-MMe-Client-Info": '<MacBookPro18,3> <Mac OS X;13.4.1;22F8> <com.apple.AOSKit/282 (com.apple.dt.Xcode/3594.4.19)>'
+        "X-MMe-Client-Info": "<MacBookPro18,3> <Mac OS X;13.4.1;22F8> <com.apple.AOSKit/282 (com.apple.dt.Xcode/3594.4.19)>",
     }
 
-    with  requests.post(
-            "https://gsa.apple.com/grandslam/GsService2",
-            headers=headers,
-            data=plist.dumps(body),
-            verify=False,
-            timeout=5,
+    with requests.post(
+        "https://gsa.apple.com/grandslam/GsService2",
+        headers=headers,
+        data=plist.dumps(body),
+        verify=False,
+        timeout=5,
     ) as resp:
         resp.raise_for_status()
 
-    response = f"HTTP-Code: {resp.status_code}\n{resp.text}"
-    logger.debug(response)
+    logger.info(f"GSA authentication response: code {resp.status_code}, {resp.text}")
 
     return plist.loads(resp.content)["Response"]
 
@@ -180,15 +188,20 @@ def generate_anisette_headers():
     with requests.get(mh_config.getAnisetteServer(), timeout=5) as response:
         response.raise_for_status()  # Hebt Fehler hervor (z. B. 404 oder 500)
         jsonResponse = response.json()
-        a = {"X-Apple-I-MD": jsonResponse["X-Apple-I-MD"],
-             "X-Apple-I-MD-M": jsonResponse["X-Apple-I-MD-M"]}
+        a = {
+            "X-Apple-I-MD": jsonResponse["X-Apple-I-MD"],
+            "X-Apple-I-MD-M": jsonResponse["X-Apple-I-MD-M"],
+        }
         a.update(generate_meta_headers(user_id=USER_ID, device_id=DEVICE_ID))
     return a
 
 
 def generate_meta_headers(serial="0", user_id=uuid.uuid4(), device_id=uuid.uuid4()):
     return {
-        "X-Apple-I-Client-Time": datetime.now(timezone.utc).replace(microsecond=0).isoformat() + "Z",
+        "X-Apple-I-Client-Time": datetime.now(timezone.utc)
+        .replace(microsecond=0)
+        .isoformat()
+        + "Z",
         "X-Apple-I-TimeZone": str(datetime.now(timezone.utc).astimezone().tzinfo),
         "loc": locale.getdefaultlocale()[0] or "en_US",
         "X-Apple-Locale": locale.getdefaultlocale()[0] or "en_US",
@@ -229,12 +242,8 @@ def decrypt_cbc(usr, data):
     return padder.update(data) + padder.finalize()
 
 
-WAITING_TIME = 60
-
-
 def sms_second_factor(dsid, idms_token):
-    identity_token = base64.b64encode(
-        (dsid + ":" + idms_token).encode()).decode()
+    identity_token = base64.b64encode((dsid + ":" + idms_token).encode()).decode()
 
     headers = {
         "User-Agent": "Xcode",
@@ -242,61 +251,38 @@ def sms_second_factor(dsid, idms_token):
         "X-Apple-Identity-Token": identity_token,
         "X-Apple-App-Info": "com.apple.gs.xcode.auth",
         "X-Xcode-Version": "11.2 (11B41)",
-        "X-Mme-Client-Info": '<MacBookPro18,3> <Mac OS X;13.4.1;22F8> <com.apple.AOSKit/282 (com.apple.dt.Xcode/3594.4.19)>'
+        "X-Mme-Client-Info": "<MacBookPro18,3> <Mac OS X;13.4.1;22F8> <com.apple.AOSKit/282 (com.apple.dt.Xcode/3594.4.19)>",
     }
 
     headers.update(generate_anisette_headers())
 
     # Extract the "boot_args" from the auth page to get the id of the trusted phone number
     pattern = r'<script.*class="boot_args">\s*(.*?)\s*</script>'
-    with requests.get("https://gsa.apple.com/auth", headers=headers, verify=False) as auth:
+    with requests.get(
+        "https://gsa.apple.com/auth", headers=headers, verify=False
+    ) as auth:
+        logger.info(f"2nd factor response: {auth.text}")
         auth.raise_for_status()
-        sms_id = 1
         match = re.search(pattern, auth.text, re.DOTALL)
-        if match:
-            boot_args = json.loads(match.group(1).strip())
-            try:
-                sms_id = boot_args["direct"]["phoneNumberVerification"]["trustedPhoneNumber"]["id"]
-            except KeyError as e:
-                logger.debug(match.group(1).strip())
-                logger.error("Key for sms id not found. Using the first phone number")
-        else:
-            logger.debug(auth.text)
-            logger.error("Script for sms id not found. Using the first phone number")
+        if not match:
+            raise Exception("unable to find 2fa parameters")
+        boot_args = json.loads(match.group(1).strip())
+        logger.info(
+            f'available phone numbers: {boot_args["direct"]["phoneNumberVerification"]["trustedPhoneNumbers"]}'
+        )
 
-        logger.info(f"Using phone with id {sms_id} for SMS2FA")
-        body = {"phoneNumber": {"id": sms_id}, "mode": "sms"}
-        for handler in logger.handlers:
-            handler.flush()
-        # Prompt for the 2FA code. It's just a string like '123456', no dashes or spaces
-        start_time = time.perf_counter()
-        code = input(
-            f"Enter SMS 2FA code (If you do not receive a code, wait {WAITING_TIME}s and press Enter. An attempt will be made to request the SMS in another way.): ")
-        end_time = time.perf_counter()
-
-    if code == "":
-        elapsed_time = int(end_time - start_time)
-        if elapsed_time < WAITING_TIME:
-            waiting_time = WAITING_TIME - elapsed_time
-            logger.info(
-                f"You only waited {elapsed_time} seconds. The next request will be started in {waiting_time} seconds")
-            time.sleep(waiting_time)
-            code = input(f"Enter SMS 2FA code if you have received it in the meantime, otherwise press Enter: ")
-
-            if code == "":
-                code = request_code(headers,sms_id)
-        else:
-            code = request_code(headers,sms_id)
-
-    body['securityCode'] = {'code': code}
+    sms_id = input("which phone number ID should be used for 2fa?")
+    body = {"phoneNumber": {"id": sms_id}, "mode": "sms"}
+    code = request_code2(headers, sms_id)
+    body["securityCode"] = {"code": code}
 
     # Send the 2FA code to Apple
-    with  requests.post(
-            "https://gsa.apple.com/auth/verify/phone/securitycode",
-            json=body,
-            headers=headers,
-            verify=False,
-            timeout=5,
+    with requests.post(
+        "https://gsa.apple.com/auth/verify/phone/securitycode",
+        json=body,
+        headers=headers,
+        verify=False,
+        timeout=5,
     ) as resp:
         resp.raise_for_status()
 
@@ -311,23 +297,42 @@ def sms_second_factor(dsid, idms_token):
         logger.info("2FA successful")
     else:
         raise Exception(
-            "2FA unsuccessful. Maybe wrong code or wrong number. Check your account details.")
+            "2FA unsuccessful. Maybe wrong code or wrong number. Check your account details."
+        )
 
 
-def request_code(headers,sms_id):
-    # This will send the 2FA code to the user's phone over SMS
-    # We don't care about the response, it's just some HTML with a form for entering the code
-    # Easier to just use a text prompt
+# This method does not work as of January 2026.
+def request_code1(headers, sms_id):
+    logger.info(f"request code method 1 headers: {headers}")
+    body = {"phoneNumber": {"id": sms_id}, "mode": "sms"}
+    logger.info(f"method 1: sending SMS via id {sms_id}")
+    with requests.put(
+        "https://gsa.apple.com/auth/verify/phone/",
+        json=body,
+        headers=headers,
+        verify=False,
+        timeout=5,
+    ) as resp:
+        logger.info(f"method 1 response: code {resp.status_code}, {resp.text}")
+        resp.raise_for_status()
+    code = input(f"Enter SMS 2FA code:")
+    return code
+
+
+# This method appears to work as of January 2026.
+def request_code2(headers, sms_id):
+    logger.info(f"request code method 2 headers: {headers}")
     logger.debug(headers)
     body = {"phoneNumber": {"id": sms_id}, "mode": "sms"}
-    logger.info(f"Sending SMS via id {sms_id}")
+    logger.info(f"method 2: sending SMS via id {sms_id}")
     with requests.put(
-            "https://idmsa.apple.com/appleauth/auth/verify/phone",
-            json=body,
-            headers=headers,
-            verify=False,
-            timeout=5
-    ) as req:
-        req.raise_for_status()
+        "https://idmsa.apple.com/appleauth/auth/verify/phone",
+        json=body,
+        headers=headers,
+        verify=False,
+        timeout=5,
+    ) as resp:
+        logger.info(f"method 2 response: code {resp.status_code}, {resp.text}")
+        resp.raise_for_status()
     code = input(f"Enter SMS 2FA code:")
     return code
